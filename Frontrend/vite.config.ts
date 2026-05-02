b127aeab4c212cb9b62f5edb648d43af4bd27a63
UdayKumarMunjam <munjamudaykumar@gmail.com>
2026-05-02 08:35:36 +0530

    fix vite base path

--- Frontrend/vite.config.ts
+++ Frontrend/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",   // 🔥 ADD THIS LINE (VERY IMPORTANT)
+
   server: {
     port: 5173,
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
